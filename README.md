def073b176921f8be422c8f53d55d79117ad0077
kfang749@gmail.com <kfang749@gmail.com>
2025-11-08 14:55:09 -0500

    Completed final changes for base mod implementation of donating gold to increase town or village prosperity.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ThePhilanthropist
+
+This mod is a flavor mod with goal of roleplaying as a philanthropist. By donating to the settlements inhabitants, we can increase the settlements prosperity.
+
+Currently, the mod only allows donation to towns and villages with a cap of 5000 for towns and 600 for villages.
+
+Plan on implementing:
+1. Donating gold to notables to increase power which will increase odds of higher tier troops
+2. Donating gold to rebuild a village after being raided
+3. Player can be ambushed somehow because his donations have attracted greedy eyes
+
--- ThePhilanthropistCampaignBehavior.cs
+++ ThePhilanthropistCampaignBehavior.cs
@@ -32,12 +32,10 @@ namespace ThePhilanthropist
 
         private void AddGameMenus(CampaignGameStarter starter)
         {
-            starter.AddGameMenuOption("town", "settlement_donation", "Donate to town.", new GameMenuOption.OnConditionDelegate(settlement_donation_on_condition),
-                new GameMenuOption.OnConsequenceDelegate(settlement_donation_on_consequence), true, -1, false, null);
-            starter.AddGameMenuOption("castle", "settlement_donation", "Donate to castle.", new GameMenuOption.OnConditionDelegate(settlement_donation_on_condition),
-                new GameMenuOption.OnConsequenceDelegate(settlement_donation_on_consequence), true, -1, false, null);
-            starter.AddGameMenuOption("village", "settlement_donation", "Donate to village.", new GameMenuOption.OnConditionDelegate(settlement_donation_on_condition),
-                new GameMenuOption.OnConsequenceDelegate(settlement_donation_on_consequence), true, -1, false, null);
+            starter.AddGameMenuOption("town", "settlement_donation", "Donate to townsfolk", new GameMenuOption.OnConditionDelegate(settlement_donation_on_condition),
+                new GameMenuOption.OnConsequenceDelegate(settlement_donation_on_consequence), false, 8, false, null);
+            starter.AddGameMenuOption("village", "settlement_donation", "Donate to villagers", new GameMenuOption.OnConditionDelegate(settlement_donation_on_condition),
+                new GameMenuOption.OnConsequenceDelegate(settlement_donation_on_consequence), false, 8, false, null);
         }
         private bool settlement_donation_on_condition(MenuCallbackArgs args)
         {
@@ -54,24 +52,24 @@ namespace ThePhilanthropist
                 "Your resolve to help these people stokes the fire within you, your desire to be good. \n\n" +
                 "You will help these people, there is no doubt. You will donate what you can.";
 
-            if ((settlement.IsTown || settlement.IsCastle) && settlement.Town.Prosperity <= 5000f)
+            if (settlement.IsTown && settlement.Town.Prosperity < 5000f)
             {
                 TextInquiryData data = new TextInquiryData("Donation", donationText, true, true, "Donate", "Cancel", new Action<string>(this.OnDonateToSettlement), 
                     null, false, new Func<string, Tuple<bool, string>>(IsDonationTextValid), "", "");
 
                 InformationManager.ShowTextInquiry(data, false, false);
             }
-            else if (settlement.IsVillage && settlement.Village.Hearth <= 600f)
+            else if (settlement.IsVillage && settlement.Village.Hearth < 600f)
             {
                 TextInquiryData data = new TextInquiryData("Donation", donationText, true, true, "Donate", "Cancel", new Action<string>(this.OnDonateToSettlement), 
-                    null, false, null, "", "");
+                    null, false, new Func<string, Tuple<bool, string>>(IsDonationTextValid), "", "");
 
                 InformationManager.ShowTextInquiry(data, false, false);
             }
             else
             {
-                string donationRejectedText = "A crowd gathers as word spreads of your intent to donate once more to the welfare of this settlement. \n\n" +
-                    "A representative of the people steps forward and says \"Ser, we cannot accept. Our settlement has flourished beyond imagination due to your donations. " +
+                string donationRejectedText = "A crowd gathers as word spreads of your intent to donate to the welfare of this settlement. \n\n" +
+                    "A representative of the people steps forward and says \"Ser, we cannot accept. Our settlement has flourished beyond imagination. " +
                     "We are content and well fed. Please spread your kindness to others as you have done here.\" \n\n" +
                     "Astonished, you look around the crowd and see in each person's face a smile that lights the sky. \n\n" +
                     "Gratified by the virtue of these people, you accept the crowds wishes and continue on for there are still those in need.";
@@ -91,18 +89,17 @@ namespace ThePhilanthropist
             Settlement settlement = Settlement.CurrentSettlement;
             float prosperityIncreaseAmount = donationAmount / 12f;
 
-            if (settlement.IsTown || settlement.IsCastle)
+            if (settlement.IsTown)
             {
-                settlement.Town.Prosperity += prosperityIncreaseAmount;
+                settlement.Town.Prosperity = settlement.Town.Prosperity + prosperityIncreaseAmount > 5000f ? 5000f : settlement.Town.Prosperity + prosperityIncreaseAmount;
             }
             else if (settlement.IsVillage)
             {
-                settlement.Village.Hearth += prosperityIncreaseAmount;
+                settlement.Village.Hearth = settlement.Village.Hearth + prosperityIncreaseAmount > 600f ? 600f : settlement.Village.Hearth + prosperityIncreaseAmount;
             }
 
             Hero.MainHero.ChangeHeroGold(-donationAmount);
-            MenuContext currentMenuContext = Campaign.Current.CurrentMenuContext;
-            currentMenuContext.Refresh();
+            Campaign.Current.CurrentMenuContext.Refresh();
         }
 
         private Tuple<bool, string> IsDonationTextValid(string text)
@@ -127,16 +124,16 @@ namespace ThePhilanthropist
                 float prosperityChange = donationAmount / 12f;
                 Settlement settlement = Settlement.CurrentSettlement;
 
-                if ((settlement.IsTown || settlement.IsCastle) && settlement.Town.Prosperity + prosperityChange > 5000f)
+                if (settlement.IsTown && settlement.Town.Prosperity + prosperityChange > 5000.0833f)
                 {
-                    float goldNeedToReachMaxProsperity = (float)Math.Floor((5000f - settlement.Town.Prosperity) * 12f);
+                    float goldNeedToReachMaxProsperity = (float)Math.Floor((5000.0833f - settlement.Town.Prosperity) * 12f);
 
                     warningText = goldNeedToReachMaxProsperity < 0f ? string.Empty : $"You can only donate {goldNeedToReachMaxProsperity}" + 
                         " {GOLD_ICON} or else the lord of this settlement will speculate on your intentions.";
                 }
-                else if (settlement.IsVillage && settlement.Village.Hearth + prosperityChange > 600f)
+                else if (settlement.IsVillage && settlement.Village.Hearth + prosperityChange > 600.0833f)
                 {
-                    float goldNeedToReachMaxProsperity = (float)Math.Floor((600f - settlement.Village.Hearth) * 12f);
+                    float goldNeedToReachMaxProsperity = (float)Math.Floor((600.0833f - settlement.Village.Hearth) * 12f);
 
                     warningText = goldNeedToReachMaxProsperity < 0f ? string.Empty : $"You can only donate {goldNeedToReachMaxProsperity}" + 
                         " {GOLD_ICON} or else the lord of this settlement will speculate on your intentions.";
